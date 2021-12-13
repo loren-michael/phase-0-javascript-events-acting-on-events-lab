d7b3b0f08d4a49ccc7056fb4c447142d13255df9
loren <lorenmichaelh@gmail.com>
2021-12-13 12:49:47 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
 const dodger = document.getElementById("dodger");
-dodger.style.backgroundColor = "#FF69B4";
 
-function moveDodgerLeft () {
+
+function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-
     if (left > 0) {
-        dodger.style.left = `${left - 1}px`;
+    dodger.style.left = `${left - 1}px`;
     }
-};
+}
 
-document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowLeft") {
-        moveDodgerLeft();
-    }
-});
-
-function moveDodgerRight () {
+function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-
     if (left < 360) {
         dodger.style.left = `${left + 1}px`;
     }
-};
-
-document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowRight") {
-        moveDodgerRight();
-    }
-});
+}
